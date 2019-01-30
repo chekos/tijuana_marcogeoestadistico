206c7f983a347ccf9980535d01815fd7ce0047bd
chekos <chekos.data@gmail.com>
2019-01-29 17:09:54 -0800

    first exploratory analysis

---
+++ notebooks/useful_funcs.py
@@ -0,0 +1,9 @@
+## A collection of useful funcs I create for this project
+
+# From RealPython's website
+def tree(directory):
+    print(f'+ {directory}')
+    for path in sorted(directory.rglob('*')):
+        depth = len(path.relative_to(directory).parts)
+        spacer = '    ' * depth
+        print(f'{spacer}+ {path.name}')
